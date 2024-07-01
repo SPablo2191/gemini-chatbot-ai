2227195d54ace9de6f399256d837509d1efcdb20
SPablo2191 <pablosandoval2191@gmail.com>
2024-07-01 11:42:24 -0300

    fix: pass data as body

--- app/main.py
+++ app/main.py
@@ -1,23 +1,28 @@
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 import google.generativeai as genai
 from dotenv import load_dotenv
 import os
-
 from app.models import GeminiQuery
 
 load_dotenv()
-print(f"mi key => {os.environ.get('GEMINI_API_KEY', '')}")
-genai.configure(api_key=os.environ.get("GEMINI_API_KEY", ""))
+api_key = os.environ.get('GEMINI_API_KEY')
+genai.configure(api_key=api_key)
 model = genai.GenerativeModel("gemini-1.5-flash")
 app = FastAPI()
-
-
-@app.get("/")
-def hello_world():
-    return {"message": "OK"}
+origins = [
+    "http://localhost:61224",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.post("/question")
-def ask_kitten(data: GeminiQuery = Depends()):
-    response = model.generate_content(data.question)
+async def ask(data: GeminiQuery):
+    response = model.generate_content(data.messageContent)
     return {"message": response.text}
--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
 from pydantic import BaseModel
 
 class GeminiQuery(BaseModel):
-    question : str
+    messageContent : str
