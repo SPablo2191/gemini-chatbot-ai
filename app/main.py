109bb5c1da8bf8c1095981d6553a8921f6855352
SPablo2191 <pablosandoval2191@gmail.com>
2024-07-15 22:26:39 -0300

    feat: config in another layer

--- app/main.py
+++ app/main.py
@@ -1,23 +1,18 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import google.generativeai as genai
-from dotenv import load_dotenv
-import os
 from app.models import GeminiQuery
+from app.config import model
 
-load_dotenv()
-api_key = os.environ.get('GEMINI_API_KEY')
-genai.configure(api_key=api_key)
-model = genai.GenerativeModel("gemini-1.5-flash")
+
 app = FastAPI()
 origins = [
-    "http://localhost:61224",
+    "*"
 ]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["POST"],
     allow_headers=["*"],
 )
 
